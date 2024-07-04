5fe40270437ca4f9b12c045d323a6c55fd292586
zhddkf <107788626+zhddkf@users.noreply.github.com>
2024-07-05 07:51:42 +0900

    sqlite 추가

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import random
+import sqlite3
 
 app = Flask(__name__)
 
@@ -9,6 +10,7 @@ def index():
     return render_template('index.html')
 
 
+# 패 선택하기
 @app.route('/play', methods=['POST'])
 def play():
     options = ['가위', '바위', '보']
@@ -19,13 +21,14 @@ def play():
     result = get_game_result(user, computer)
 
     return render_template('result.html', user=user, computer=computer, result=result)
+#--- sql코드 추가한 후로 23번 라인이 오류 남
 
-
+# 승부규칙
 def get_game_result(u, c):
     win=0
     draw=0
     lose=0
-    
+
     if u == c:
         draw+=1
         return '비겼습니다'
@@ -36,6 +39,34 @@ def get_game_result(u, c):
         lose+=1
         return '졌습니다'
     
+
+# 데이터베이스 선언
+def init_db():
+    connect=sqlite3.connect('score.db')
+    c=connect.cursor()
+    c.execute('''create table SCORE
+                 (id char(10), user TEXT, computer TEXT, result TEXT)''')
+    connect.commit()
+    connect.close()
+
+# db에 승부 입력
+def save_result(user, computer, result):
+    connect=sqlite3.connect('score.db')
+    c=connect.cursor()
+    c.execute('insert into score (user, computer, result) \
+              values (?, ?, ?)', user, computer, result)
+    connect.commit()
+    connect.close()
+
+
+# 전적 조회
+def check_score():
+    connect=sqlite3.connect('score.db')
+    c=connect.cursor()
+    c.execute('select * from score order by id desc')
+    score=c.fetchall()
+    connect.close()
+    return score
 
 
 if __name__ == '__main__':
